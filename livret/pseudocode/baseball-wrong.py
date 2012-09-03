bbad550198f12caade08f050309faa9e4be2161d
Martin Quinson <martin.quinson@loria.fr>
2012-09-03 17:04:14 +0200

    more stats

--- livret/pseudocode/baseball-wrong.py
+++ livret/pseudocode/baseball-wrong.py
@@ -4,6 +4,13 @@ from itertools import permutations as perms
 
 # etat: [(couleur base, couleur pion)]
 done = {} # -1: loop, 0: in processing, 1: finish
+
+# for the debug messages: value in hash "done" -> string representation
+outcome={}
+outcome[-1]="looping"
+outcome[0]="ongoing"
+outcome[1]="OK"
+
 def compute(nb_places, nb_bases, etat):
   if (etat in done):
     if done[etat]==1:
@@ -39,8 +46,8 @@ def compute(nb_places, nb_bases, etat):
   return res
 
 if __name__=='__main__':
-  nb_bases = 5
-  nb_places = 2
+  nb_bases = 4
+  nb_places = 3
   all_states = set(tuple(sorted((i%nb_bases, p) 
                               for i, p in zip(range(nb_places*nb_bases),
                                               perm)))
@@ -50,18 +57,26 @@ if __name__=='__main__':
   for e in all_states:
       compute(nb_places, nb_bases, e)
       
+  print "Nb bases:",nb_bases,"; Nb colors:",nb_places,"; Nb states:", len(all_states)
+  all_res = Counter(done[e] for e in all_states)
   # Prepare the filtering
   initial_states = all_states 
   # filter out all states where someone is already home: we never take such states as initial states
   initial_states = filter(lambda x: all(b!=p for b,p in x), initial_states)
+  res_nobody_home = Counter(done[e] for e in initial_states)
+  
   # filter out all states where 2 guys of the same color are at the same base: we never take such states as initial states  
   initial_states = filter(lambda e: filter(lambda x: x[1]==2 and x[0]!=x[1], Counter(e).items()), initial_states)
+  res_nobody_home_singletons = Counter(done[e] for e in initial_states)
+
+  singletons_states = filter(lambda e: filter(lambda x: x[1]==2 and x[0]!=x[1], Counter(e).items()), all_states)
+  res_singleton = Counter(done[e] for e in singletons_states)
 
   res = Counter(done[e]
                 for e in initial_states)
-  outcome={}
-  outcome[-1]="looping"
-  outcome[1]="OK"
-  print '\n'.join( str(e)+": "+str(outcome[done[e]]) for e in initial_states)
+#  print '\n'.join( str(e)+": "+str(outcome[done[e]]) for e in initial_states)
               
-  print "Loop:",res[-1],", No loop:",res[1]," => Looping in ",( float(res[-1])/(res[-1]+res[1])*100),"% of cases"
+  print "All states:  Loop:",all_res[-1],", No loop:",all_res[1]," => Looping in ",( float(all_res[-1])/(all_res[-1]+all_res[1])*100),"% of cases"
+  print "Nobody Home:  Loop:",res_nobody_home[-1],", No loop:",res_nobody_home[1]," => Looping in ",( float(res_nobody_home[-1])/(res_nobody_home[-1]+res_nobody_home[1])*100),"% of cases"
+  print "Singleton:  Loop:",res_singleton[-1],", No loop:",res_singleton[1]," => Looping in ",( float(res_singleton[-1])/(res_singleton[-1]+res_singleton[1])*100),"% of cases"
+  print "Nobody Home + Singleton:  Loop:",res_nobody_home_singletons[-1],", No loop:",res_nobody_home_singletons[1]," => Looping in ",( float(res_nobody_home_singletons[-1])/(res_nobody_home_singletons[-1]+res_nobody_home_singletons[1])*100),"% of cases"
