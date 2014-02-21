5550af9b98b953ca694afbdf2ac310d72e618ab0
Martin Quinson <martin.quinson@loria.fr>
2014-02-21 15:38:18 +0100

    reorganise les choses autour des algos (le livret reste à integrer)

--- algo1/petit-livre.sh
+++ algo1/petit-livre.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-if [ $# -le 0 ] ; then
-   echo "Usage: petit-livre fichier"
+if [ $# -le 1 ] ; then
+   echo "Usage: petit-livre input output"
    exit 1
 fi
 
@@ -16,7 +16,7 @@ cp "$1" $tmpdir
 cd $tmpdir
 pdfjam --outfile A.pdf --papersize {84cm,297mm} --nup 4x1 --angle 180 "$1" 7,6,5,4
 pdfjam --outfile B.pdf --papersize {84cm,297mm} --nup 4x1             "$1" 8,1,2,3
-pdfjam --outfile "$pwdir/$basname"-livret.pdf --landscape --nup 1x2 A.pdf B.pdf
+pdfjam --outfile "$pwdir/$2" --landscape --nup 1x2 A.pdf B.pdf
 
 cd $pwdir
 rm -r $tmpdir
