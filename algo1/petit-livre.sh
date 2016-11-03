804b82000d5a3cdedd13ce1c0db20c3cb5b3686c
jcb <jeanchristophe.bach@inria.fr>
2016-11-03 11:03:41 +0100

    fix missing quotes in pdfjam --papersize parameter

--- algo1/petit-livre.sh
+++ algo1/petit-livre.sh
@@ -14,8 +14,8 @@ basname=`basename $1 .pdf`
 cp "$1" $tmpdir
 
 cd $tmpdir
-pdfjam --outfile A.pdf --papersize {84cm,297mm} --nup 4x1 --angle 180 "$1" 7,6,5,4
-pdfjam --outfile B.pdf --papersize {84cm,297mm} --nup 4x1             "$1" 8,1,2,3
+pdfjam --outfile A.pdf --papersize '{84cm,297mm}' --nup 4x1 --angle 180 "$1" 7,6,5,4
+pdfjam --outfile B.pdf --papersize '{84cm,297mm}' --nup 4x1             "$1" 8,1,2,3
 pdfjam --outfile "$pwdir/$2" --landscape --nup 1x2 A.pdf B.pdf
 
 cd $pwdir
